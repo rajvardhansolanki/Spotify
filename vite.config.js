66b17ba60472557d165a74f87ce9cb1f8c13257d
Rajvardhan Solanki <rajvardhansolanki0@gmail.com>
2025-09-04 15:00:51 +0530

    minor changes added in configration file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Ensure root path for Netlify deployment
   plugins: [
-    tailwindcss(),react()],
+    react(),
+    tailwindcss(),
+  ],
 })
