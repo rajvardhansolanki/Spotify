fe5f0a5b7d00dc55e6b7cfe8ac803d2ad795a65c
Rajvardhan Solanki <rajvardhansolanki0@gmail.com>
2025-09-04 19:39:25 +0530

    tiles added on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import RowCard from "../../components/RowCard/RowCard";
 
 const Home = () => {
     return (
-        <div className="w-full bg-[#000000] sm:p-6 p-3 mt-0 sm:rounded-xl rounded-none shadow-md">
+        <div className="w-full bg-[#000000] sm:p-6 p-3 pt-6 pb-6 sm:rounded-xl rounded-none shadow-md">
             <RowCard />
         </div>
     );
